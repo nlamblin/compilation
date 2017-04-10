84842485449931359217768fc1d82e116319ed30
Michael Lou <michael.lou9@etu.univ-lorraine.fr>
2017-04-10 11:42:30 +0200

    Parcours

--- src/fr/ul/miage/exemple/Arbre.java
+++ src/fr/ul/miage/exemple/Arbre.java
@@ -1,5 +1,6 @@
 package fr.ul.miage.exemple;
 
+import com.sun.xml.internal.messaging.saaj.packaging.mime.internet.ParseException;
 
 public class Arbre{
 	
@@ -16,6 +17,16 @@ public class Arbre{
 	
 	public String afficher(){
 		return this.racine.toString()+"\n";
+	}
+	
+	
+	public String parcourirTousLesFils(Noeud n, TableDesSymboles tds) throws ParseException{
+		String res = "";
+		for(Noeud noeud : n.fils){
+			System.out.println(noeud.cle);
+			parcourirTousLesFils(noeud,tds);
+		}
+		return res;
 	}
 	
 }
--- src/fr/ul/miage/exemple/Main.java
+++ src/fr/ul/miage/exemple/Main.java
@@ -27,16 +27,16 @@ public class Main {
 		FileInputStream file;
 		ParserCup parser;
 		
-		System.out.println("----------00-syntaxe----------");
-		file = new FileInputStream(new File("test-suite/00-syntaxe.miage"));
-		parser = new ParserCup(new Yylex(new BufferedReader(new InputStreamReader(file))));
-		try {
-			parser.parse();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		System.out.println("----------00-syntaxe----------");
-		Thread.sleep(1000);
+//		System.out.println("----------00-syntaxe----------");
+//		file = new FileInputStream(new File("test-suite/00-syntaxe.miage"));
+//		parser = new ParserCup(new Yylex(new BufferedReader(new InputStreamReader(file))));
+//		try {
+//			parser.parse();
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
+//		System.out.println("----------00-syntaxe----------");
+//		Thread.sleep(1000);
 //
 //		System.out.println("----------01-minimal----------");
 //		file = new FileInputStream(new File("test-suite/01-minimal.miage"));
@@ -60,16 +60,16 @@ public class Main {
 //		System.out.println("----------02-global----------");
 //		Thread.sleep(1000);
 //
-//		System.out.println("----------03-expression----------");
-//		file = new FileInputStream(new File("test-suite/03-expression.miage"));
-//		parser = new ParserCup(new Yylex(new BufferedReader(new InputStreamReader(file))));
-//		try {
-//			parser.parse();
-//		} catch (Exception e) {
-//			e.printStackTrace();
-//		}
-//		System.out.println("----------03-expression----------");
-//		Thread.sleep(1000);
+		System.out.println("----------03-expression----------");
+		file = new FileInputStream(new File("test-suite/03-expression.miage"));
+		parser = new ParserCup(new Yylex(new BufferedReader(new InputStreamReader(file))));
+		try {
+			parser.parse();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		System.out.println("----------03-expression----------");
+		Thread.sleep(1000);
 //
 //		System.out.println("----------04-expression----------");
 //		file = new FileInputStream(new File("test-suite/04-expression.miage"));
