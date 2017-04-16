075973d19b8363a82906adaa3c71ba8dc64f4bea
nicolas <nicolas.lamblin@outlook.com>
2017-04-16 19:26:40 +0200

    Avancement assembleur, ajout de la condition if, des instructions br et bf

--- src/fr/ul/miage/exemple/Affect.java
+++ src/fr/ul/miage/exemple/Affect.java
@@ -5,4 +5,8 @@ public class Affect {
 	public void genererAffectConstante(String variable, int valeur) {
 		Assembleur.chaineAssembleur += variable + ": LONG("+valeur+") \n";
 	}
+	
+	public void genererAffect(String numeroRegistre1, String numeroRegistre2) {
+		Assembleur.chaineAssembleur += "MOV("+numeroRegistre2+","+numeroRegistre1+")";
+	}
 }
---
+++ src/fr/ul/miage/exemple/Bf.java
@@ -0,0 +1,8 @@
+package fr.ul.miage.exemple;
+
+public class Bf {
+
+	public void genererBf(String intitule) {
+		Assembleur.chaineAssembleur += "BF("+intitule+")";
+	}
+}
