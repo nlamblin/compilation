0720751ffff536848b7ccfa1ccd68b8b9d26f43e
nicolas <nicolas.lamblin@outlook.com>
2017-04-18 16:12:52 +0200

    Modif de l'assembleur pour l'affectation

--- src/fr/ul/miage/exemple/Affect.java
+++ src/fr/ul/miage/exemple/Affect.java
@@ -2,11 +2,11 @@ package fr.ul.miage.exemple;
 
 public class Affect {
 
-	public void genererAffectConstante(String variable, int valeur) {
-		Assembleur.chaineAssembleur += variable + ": LONG("+valeur+") \n";
-	}
-	
 	public void genererAffect(String numeroRegistre1, String numeroRegistre2) {
 		Assembleur.chaineAssembleur += "MOV("+numeroRegistre2+","+numeroRegistre1+") \n";
+	}
+	
+	public void genererAffectConstante(String numeroRegistre, int constante) {
+		Assembleur.chaineAssembleur += "CMOVE("+constante+","+numeroRegistre+") \n";
 	}
 }
