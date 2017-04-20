714cbecc2343af36f8d6b27dea3a18ea9ab37530
Valentin Nourdin <valentin.nourdin@hotmail.fr>
2017-04-20 15:29:56 +0200

    Correction et modification affichage arbre

--- src/fr/ul/miage/exemple/Arbre.java
+++ src/fr/ul/miage/exemple/Arbre.java
@@ -1,127 +1,123 @@
 package fr.ul.miage.exemple;
 
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.TreeMap;
-
 import com.sun.xml.internal.messaging.saaj.packaging.mime.internet.ParseException;
+
+import java.util.*;
 
 public class Arbre {
 
-	public Noeud racine;
-	public ArrayList<Map<String, String[]>> liste_element;
-	public Map<String, String[]> map;
+    public Noeud racine;
+    public ArrayList<Map<String, String[]>> liste_element;
+    public Map<String, String[]> map;
 
-	public Arbre() {
-		this.racine = new Noeud("Program", null);
-		this.liste_element = new ArrayList<>();
-		this.map = new HashMap<>();
-	}
+    public Arbre() {
+        this.racine = new Noeud("Program", null);
+        this.liste_element = new ArrayList<>();
+        this.map = new HashMap<>();
+    }
 
-	public void ajouterNoeud(Noeud n) {
-		this.racine.fils.add(n);
-	}
+    public void ajouterNoeud(Noeud n) {
+        this.racine.fils.add(n);
+    }
 
-	public String afficher() {
-		return this.racine.toString() + "\n";
-	}
+    public String afficher() {
+        return this.racine.afficher(1) + "\n";
+    }
 
-	public void parcourirTousLesFils(Noeud n, TableDesSymboles tds, int i, String parent) throws ParseException{
-		String fils = "";
-		int j = i;
-		for (Noeud no : n.fils) {
-			String[] tab = new String[2];
-			if(no.cle.equals("FUNCTION")){
-				parent = "" + i;
-				String nom = tds.getNom(no.valeur);
-				tab[0] = nom;
-				String valeur = tds.getValeur(nom);
-				tab[1] = valeur;
-				this.map.put(parent, tab);
-			}
-			switch_parcours(no, tab, j, parent,tds);
-			if(no.fils.size()>0){
-				for (Noeud noe : no.fils) {
-					String[] tab2 = new String[2];
-					j = j + 1;
-					fils = parent + j;
-					switch_parcours(noe, tab2, j, fils, tds);			
-				}
-				j=0;
-			}else{
-				j++;
-			}
-			i = i + 1;
-		}
-		this.map = this.doSort(this.map);
-	}
+    public void parcourirTousLesFils(Noeud n, TableDesSymboles tds, int i, String parent) throws ParseException {
+        String fils = "";
+        int j = i;
+        for (Noeud no : n.fils) {
+            String[] tab = new String[2];
+            if (no.cle.equals("FUNCTION")) {
+                parent = "" + i;
+                String nom = tds.getNom(no.valeur);
+                tab[0] = nom;
+                String valeur = tds.getValeur(nom);
+                tab[1] = valeur;
+                this.map.put(parent, tab);
+            }
+            switch_parcours(no, tab, j, parent, tds);
+            if (no.fils.size() > 0) {
+                for (Noeud noe : no.fils) {
+                    String[] tab2 = new String[2];
+                    j = j + 1;
+                    fils = parent + j;
+                    switch_parcours(noe, tab2, j, fils, tds);
+                }
+                j = 0;
+            } else {
+                j++;
+            }
+            i = i + 1;
+        }
+        this.map = this.doSort(this.map);
+    }
 
-	public void switch_parcours(Noeud no, String[] tab, int i,String parent, TableDesSymboles tds) throws ParseException {
-		String nom = "";
-		String valeur = "";
-		String fils = "";
-		switch (no.cle) {
-		case "AFFECT":
-			tab[0] = no.cle;
-			tab[1] = no.valeur;
-			this.map.put(parent, tab);
-			parcourirTousLesFils(no, tds, i, parent);
-			break;
-		case "CALL":
+    public void switch_parcours(Noeud no, String[] tab, int i, String parent, TableDesSymboles tds) throws ParseException {
+        String nom = "";
+        String valeur = "";
+        String fils = "";
+        switch (no.cle) {
+            case "AFFECT":
+                tab[0] = no.cle;
+                tab[1] = no.valeur;
+                this.map.put(parent, tab);
+                parcourirTousLesFils(no, tds, i, parent);
+                break;
+            case "CALL":
 
-			break;
-		case "CONSTANTE":
-			fils = parent + i;
-			tab[0] = no.cle;
-			tab[1] = no.valeur;
-			this.map.put(fils, tab);
-			break;
-		case "VAR":
-			fils = parent + i;
-			nom = tds.getNom(no.valeur);
-			tab[0] = nom;
-			valeur = tds.getValeur(nom);
-			tab[1] = valeur;
-			this.map.put(fils, tab);
-			break;
-		case "+":
-			fils = parent + i;
-			tab[0] = no.cle;
-			tab[1] = no.valeur;
-			this.map.put(fils, tab);
-			parcourirTousLesFils(no, tds, 1, fils);
-			break;
-		case "-":
+                break;
+            case "CONSTANTE":
+                fils = parent + i;
+                tab[0] = no.cle;
+                tab[1] = no.valeur;
+                this.map.put(fils, tab);
+                break;
+            case "VAR":
+                fils = parent + i;
+                nom = tds.getNom(no.valeur);
+                tab[0] = nom;
+                valeur = tds.getValeur(nom);
+                tab[1] = valeur;
+                this.map.put(fils, tab);
+                break;
+            case "+":
+                fils = parent + i;
+                tab[0] = no.cle;
+                tab[1] = no.valeur;
+                this.map.put(fils, tab);
+                parcourirTousLesFils(no, tds, 1, fils);
+                break;
+            case "-":
 
-			break;
-		case "/":
-			fils = parent + i;
-			tab[0] = no.cle;
-			tab[1] = no.valeur;
-			this.map.put(fils, tab);
-			parcourirTousLesFils(no, tds, 1, fils);
-			break;
-		case "*":
+                break;
+            case "/":
+                fils = parent + i;
+                tab[0] = no.cle;
+                tab[1] = no.valeur;
+                this.map.put(fils, tab);
+                parcourirTousLesFils(no, tds, 1, fils);
+                break;
+            case "*":
 
-			break;
-		case "FUNC":
+                break;
+            case "FUNC":
 
-			break;
-		}
-	}
+                break;
+        }
+    }
 
-	public void afficherListeElement() throws ParseException {
-		for (Map.Entry<String, String[]> e : this.map.entrySet()) {
-			String val = "";
-			if(e.getValue()[1] != null){
-				val =" valeur :" + e.getValue()[1];
-			}		
-			System.out.println(e.getKey() + "  " + e.getValue()[0] + val);
-		}
-		/*
-		 * switch (s[0]) { case "VAR" : Var v = new Var(); //
+    public void afficherListeElement() throws ParseException {
+        for (Map.Entry<String, String[]> e : this.map.entrySet()) {
+            String val = "";
+            if (e.getValue()[1] != null) {
+                val = " valeur :" + e.getValue()[1];
+            }
+            System.out.println(e.getKey() + "  " + e.getValue()[0] + val);
+        }
+        /*
+         * switch (s[0]) { case "VAR" : Var v = new Var(); //
 		 * System.out.println(tds.getValeur(tds.getNom(tab[1])));
 		 * v.genererVar(valeur); case "FUNCTION" : System.out.println(
 		 * "identification d'une fonction"); case "AFFECT" : System.out.println(
@@ -131,25 +127,23 @@ public class Arbre {
 		 * System.out.println(valeur); default : break; }
 		 */
 
-		// System.out.println(s[0] + " " + s[1]);
-		// System.out.println(nom + " " + type + " " + categorie + " " +
-		// valeur);
-	}
+        // System.out.println(s[0] + " " + s[1]);
+        // System.out.println(nom + " " + type + " " + categorie + " " +
+        // valeur);
+    }
 
-	public Map<String, String[]> doSort(Map<String, String[]> map) {
-		Comparator<String> comparator = new KeyComparator<String>();
-		Map<java.lang.String, java.lang.String[]> sortedMap = new TreeMap<String, String[]>(comparator);
-		sortedMap.putAll(map);
-		return sortedMap;
-	}
+    public Map<String, String[]> doSort(Map<String, String[]> map) {
+        Comparator<String> comparator = new KeyComparator<String>();
+        Map<java.lang.String, java.lang.String[]> sortedMap = new TreeMap<String, String[]>(comparator);
+        sortedMap.putAll(map);
+        return sortedMap;
+    }
 
-	public class KeyComparator<String> implements Comparator<String> {
+    public class KeyComparator<String> implements Comparator<String> {
 
-		@Override
-		public int compare(String sourceKey, String targetKey) {
-			return ((java.lang.String) sourceKey).compareTo((java.lang.String) targetKey);
-		}
-
-	}
-
+        @Override
+        public int compare(String sourceKey, String targetKey) {
+            return ((java.lang.String) sourceKey).compareTo((java.lang.String) targetKey);
+        }
+    }
 }
--- src/fr/ul/miage/exemple/Noeud.java
+++ src/fr/ul/miage/exemple/Noeud.java
@@ -4,50 +4,39 @@ import java.util.ArrayList;
 
 public class Noeud {
 
-	public static int numeroNoeud = 0;
-	public String cle, valeur;
-	public ArrayList<Noeud> fils;
-	
-	public Noeud(String c, String v){
-		this.cle = c;
-		this.valeur = v;
-		this.fils = new ArrayList<>();
-		this.numeroNoeud++;
-	}
-	
-	public void setCle(String c){
-		this.cle = c;
-	}
-	
-	public void setValeur(String v){
-		this.valeur = v;
-	}
-	
-	public String toString(){
-		String res = "-";
-		String val = "";
-		if(this.valeur == null){
-			val="$";
-		}else{
-			val = this.valeur;
-		}
-		res += "||"+this.cle + "|" +val+"||";
-		
-		if(this.fils.size() > 0){
-			res += "\n";
-			for(Noeud n : this.fils){
-				int nb_esp = 1;
-				for(int j = 0; j<nb_esp;j++){					
-					res += "\n";
-					res += " ";
-				}
-				res += "  " + n.toString();
-				nb_esp++;
-			}
+    public static int numeroNoeud = 0;
+    public String cle, valeur;
+    public ArrayList<Noeud> fils;
 
-			res += "\n";
-			
-		}
-		return res ;
-	}
+    public Noeud(String c, String v) {
+        this.cle = c;
+        this.valeur = v;
+        this.fils = new ArrayList<>();
+        this.numeroNoeud++;
+    }
+
+    public void setCle(String c) {
+        this.cle = c;
+    }
+
+    public void setValeur(String v) {
+        this.valeur = v;
+    }
+
+    public String afficher(int niveau) {
+        String val = (this.valeur == null) ? "" : " : " + this.valeur;
+        String res = "< " + this.cle + val + " >";
+
+        if (!this.fils.isEmpty()) {
+            String tabNiveau = "";
+            for (int i = 0; i < niveau - 1; i++)
+                tabNiveau += "\t";
+
+            for (Noeud noeud : this.fils)
+                res += "\n" + tabNiveau + "\t" + noeud.afficher(niveau + 1);
+
+            res += "\n" + tabNiveau + "< fin " + this.cle + " >";
+        }
+        return res;
+    }
 }
