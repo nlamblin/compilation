07ba45b784832e17cdbcfc17e92d7378660cf60d
Valentin Nourdin <valentin.nourdin@hotmail.fr>
2017-04-23 21:54:17 +0200

    Mise en forme du code assembleur en sortie

--- src/fr/ul/miage/exemple/Assembleur.java
+++ src/fr/ul/miage/exemple/Assembleur.java
@@ -9,9 +9,9 @@ import java.io.IOException;
 
 public class Assembleur {
 
-    public TableDesSymboles tds;
-    public int nbWhile;
-    public int nbIf;
+    private TableDesSymboles tds;
+    private int nbWhile;
+    private int nbIf;
 
     /**
      * Constructeur
@@ -21,12 +21,10 @@ public class Assembleur {
         this.nbIf = 0;
     }
 
-
     public void genererFichier(String chaine) {
         try {
             BufferedWriter writer = new BufferedWriter(new FileWriter(new File("assembleur.txt")));
             writer.write(chaine);
-            System.out.println(chaine);
             writer.close();
         } catch (IOException e) {
             e.printStackTrace();
@@ -45,13 +43,13 @@ public class Assembleur {
                 + "CALL(main) \n "
                 + "HALT() \n"
                 + genererData(tds) + "\n"
-                + genererCode(racine) + "\n"
+                + genererCode(racine)
                 + "pile:\n";
 
         return res;
     }
 
-    public String genererData(TableDesSymboles tds) {
+    private String genererData(TableDesSymboles tds) {
         String res = "data : \n";
         String[][] table = tds.table;
         // parcours de la table des symboles
@@ -61,10 +59,11 @@ public class Assembleur {
                 res += "\t" + table[i][1] + ": LONG(" + table[i][4] + ") \n";
             }
         }
+
         return res;
     }
 
-    public String genererCode(Noeud racine) throws ParseException {
+    private String genererCode(Noeud racine) throws ParseException {
         String res = "code : \n";
         // parcours de tous les fils
         for (Noeud n : racine.fils) {
@@ -74,34 +73,17 @@ public class Assembleur {
         return res;
     }
 
-    public String genererFonction(Noeud noeud) throws ParseException {
-        String res = "\n" + tds.getNom(noeud.valeur) + ":\n";
+    private String genererFonction(Noeud noeud) throws ParseException {
+        String res = "\n" + tds.getNom(noeud.valeur) + ": \n";
         for (Noeud n : noeud.fils) {
             res += genererInstructions(n);
         }
-        res += "fin_" + tds.getNom(noeud.valeur) + ":\n";
+        res += "fin_" + tds.getNom(noeud.valeur) + ": \n\n";
 
         return res;
     }
 
-    public String genererReturn(Noeud noeud) throws ParseException {
-        String res = "";
-
-        if (noeud.fils.isEmpty()) {
-            // pas sur du tout de ça
-            int b = (tds.getParametre(tds.getNom(noeud.valeur)) - 2) * 4;
-
-            // trouver un moyen mais pas changer genererExpression !!!
-            res += genererExpression(noeud.fils.get(0))
-                    + "POP(R0)"
-                    + "PUTFRAME(R0, " + b + ")";
-        }
-        res += "";
-
-        return res;
-    }
-
-    public String genererInstructions(Noeud noeud) throws ParseException {
+    private String genererInstructions(Noeud noeud) throws ParseException {
         String res = "";
 
         switch (noeud.cle) {
@@ -124,15 +106,15 @@ public class Assembleur {
         return res;
     }
 
-    public String genererAffectation(Noeud noeud) throws ParseException {
+    private String genererAffectation(Noeud noeud) throws ParseException {
         String res = genererExpression(noeud.fils.get(1));
-        System.out.println(this.tds.getNom(noeud.fils.get(0).valeur));
+
         res += "POP(R0) \n"
                 + "ST(R0," + this.tds.getNom(noeud.fils.get(0).valeur) + ") \n";
-        return res + "\n";
+        return res;
     }
 
-    public String genererExpression(Noeud noeud) throws ParseException {
+    private String genererExpression(Noeud noeud) throws ParseException {
         String res = "";
 
         switch (noeud.cle) {
@@ -145,90 +127,130 @@ public class Assembleur {
                         + "PUSH(R0) \n";
                 break;
             case "+":
-                res += genererExpression(noeud.fils.get(0)) + "\n"
-                        + genererExpression(noeud.fils.get(1)) + "\n"
+                res += genererExpression(noeud.fils.get(0))
+                        + genererExpression(noeud.fils.get(1))
                         + "POP(R2) \n"
                         + "POP(R1) \n"
                         + "ADD(R1,R2,R0) \n"
                         + "PUSH(R0) \n";
                 break;
             case "-":
-                res += genererExpression(noeud.fils.get(0)) + "\n"
-                        + genererExpression(noeud.fils.get(1)) + "\n"
+                res += genererExpression(noeud.fils.get(0))
+                        + genererExpression(noeud.fils.get(1))
                         + "POP(R2) \n"
                         + "POP(R1) \n"
                         + "SUB(R1,R2,R0) \n"
                         + "PUSH(R0) \n";
                 break;
             case "*":
-                res += genererExpression(noeud.fils.get(0)) + "\n"
-                        + genererExpression(noeud.fils.get(1)) + "\n"
+                res += genererExpression(noeud.fils.get(0))
+                        + genererExpression(noeud.fils.get(1))
                         + "POP(R2) \n"
                         + "POP(R1) \n"
                         + "MUL(R1,R2,R0) \n"
                         + "PUSH(R0) \n";
                 break;
             case "/":
-                res += genererExpression(noeud.fils.get(0)) + "\n"
-                        + genererExpression(noeud.fils.get(1)) + "\n"
+                res += genererExpression(noeud.fils.get(0))
+                        + genererExpression(noeud.fils.get(1))
                         + "POP(R2) \n"
                         + "POP(R1) \n"
                         + "DIV(R1,R2,R0) \n"
                         + "PUSH(R0) \n";
                 break;
         }
-
-        return res + "\n";
+        return res;
     }
 
-    public String genererCondition(Noeud noeud) throws ParseException {
+    private String genererReturn(Noeud noeud) throws ParseException {
+        String res = "";
+
+        if (noeud.fils.isEmpty()) {
+            // pas sur du tout de ça
+            int b = (tds.getParametre(tds.getNom(noeud.valeur)) - 2) * 4;
+
+            // trouver un moyen mais pas changer genererExpression !!!
+            res += genererExpression(noeud.fils.get(0))
+                    + "POP(R0) \n"
+                    + "PUTFRAME(R0, " + b + ") \n";
+        }
+        return res;
+    }
+
+    private String genererWhile(Noeud noeud) throws ParseException {
+        this.nbWhile++;
+        String res = "\nwhile_" + this.nbWhile + ": \n"
+                + genererCondition(noeud.fils.get(0))
+                + "POP(R0) \n"
+                + "BF(R0, done_" + this.nbWhile + ") \n"
+                + genererBloc(noeud.fils.get(1))
+                + "BF(R0, while_" + this.nbWhile + ") \n"
+                + "done_" + this.nbWhile + ": \n\n";
+        return res;
+    }
+
+    private String genererIf(Noeud noeud) throws ParseException {
+        this.nbIf++;
+        String res = "\nif_" + this.nbIf + ": \n"
+                + genererCondition(noeud.fils.get(0))
+                + "POP(R0) \n"
+                + "BF(R0, else_" + this.nbIf + ") \n"
+                + genererBloc(noeud.fils.get(1))
+                + "BR(fsi_" + this.nbIf + ") \n"
+                + "\nelse_" + this.nbIf + ": \n"
+                + genererBloc(noeud.fils.get(2))
+                + "fsi_" + this.nbIf + ": \n\n";
+        return res;
+    }
+
+    private String genererCondition(Noeud noeud) throws ParseException {
         String res = "";
 
         System.out.println(noeud.fils.get(1).cle);
         switch (noeud.fils.get(1).cle) {
             case "<":
-                res += genererExpression(noeud.fils.get(0)) + " \n"
-                        + genererExpression(noeud.fils.get(2)) + " \n"
+                res += genererExpression(noeud.fils.get(0))
+                        + genererExpression(noeud.fils.get(2))
                         + "POP(R2) \n"
                         + "POP(R1) \n"
                         + "CMPLT(R1,R2,R0) \n"
                         + "PUSH(R0) \n";
                 break;
             case ">":
-                res += genererExpression(noeud.fils.get(0)) + " \n"
-                        + genererExpression(noeud.fils.get(2)) + " \n"
+                res += genererExpression(noeud.fils.get(0))
+                        + genererExpression(noeud.fils.get(2))
                         + "POP(R2) \n"
                         + "POP(R1) \n"
                         + "CMPLT(R2,R1,R0) \n"
                         + "PUSH(R0) \n";
                 break;
             case "<=":
-                res += genererExpression(noeud.fils.get(0)) + " \n"
-                        + genererExpression(noeud.fils.get(2)) + " \n"
+                res += genererExpression(noeud.fils.get(0))
+                        + genererExpression(noeud.fils.get(2))
                         + "POP(R2) \n"
                         + "POP(R1) \n"
                         + "CMPLE(R1,R2,R0) \n"
                         + "PUSH(R0) \n";
                 break;
             case ">=":
-                res += genererExpression(noeud.fils.get(0)) + " \n"
-                        + genererExpression(noeud.fils.get(2)) + " \n"
+                res += genererExpression(noeud.fils.get(0))
+                        + genererExpression(noeud.fils.get(2))
                         + "POP(R2) \n"
                         + "POP(R1) \n"
                         + "CMPLE(R2,R1,R0) \n"
                         + "PUSH(R0) \n";
                 break;
             case "=":
-                res += genererExpression(noeud.fils.get(0)) + " \n"
-                        + genererExpression(noeud.fils.get(2)) + " \n"
+                res += genererExpression(noeud.fils.get(0))
+                        + genererExpression(noeud.fils.get(2))
                         + "POP(R2) \n"
                         + "POP(R1) \n"
                         + "CMPEQ(R1,R2,R0) \n"
                         + "PUSH(R0) \n";
                 break;
             case "!=":
-                res += genererExpression(noeud.fils.get(0)) + " \n"
-                        + genererExpression(noeud.fils.get(2)) + " \n"
+                res += genererExpression(noeud.fils.get(0))
+                        + genererExpression(noeud.fils.get(2))
                         + "POP(R2) \n"
                         + "POP(R1) \n"
                         + "CMPEQ(R1,R2,R0) \n"
@@ -237,42 +259,16 @@ public class Assembleur {
             default:
                 break;
         }
-        return res + "\n";
+        return res;
     }
 
-    public String genererIf(Noeud noeud) throws ParseException {
-        this.nbIf++;
-        String res = "if_" + this.nbIf + ":"
-                + genererCondition(noeud.fils.get(0)) + " \n"
-                + "POP(R0) \n"
-                + "BF(R0, else_" + this.nbIf + ") \n"
-                + genererBloc(noeud.fils.get(1))
-                + "BR(fsi_" + this.nbIf + ") \n"
-                + "else_" + this.nbIf + ": \t"
-                + genererBloc(noeud.fils.get(2))
-                + "fsi_" + this.nbIf + ": \n";
-        return res + "\n";
-    }
-
-    public String genererWhile(Noeud noeud) throws ParseException {
-        this.nbWhile++;
-        String res = "while_" + this.nbWhile + ":"
-                + genererCondition(noeud.fils.get(0))
-                + "POP(R0) \n"
-                + "BF(R0, done_" + this.nbWhile + ") \n"
-                + genererBloc(noeud.fils.get(1))
-                + "BF(R0, while_" + this.nbWhile + ") \n"
-                + "done_" + this.nbWhile + ":";
-        return res + "\n";
-    }
-
-    public String genererBloc(Noeud noeud) throws ParseException {
+    private String genererBloc(Noeud noeud) throws ParseException {
         String res = "";
 
         for (Noeud n : noeud.fils) {
             res += genererInstructions(n);
         }
 
-        return res + "\n";
+        return res;
     }
 }
